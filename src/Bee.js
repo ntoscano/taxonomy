4d6b5a52f5fdc6b9df04c4f89a7102c2d6560477
Hack Reactor Students <accounts@hackreactor.com>
2015-07-08 15:36:20 -0700

    Complete taxonomy basic requirements

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  Grub.call(this); // the contents of Grub are being applied to the context of an instantiation of a bee
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function(){ // use .prototype so that potential later modifications to "givehoney" affect all instances
+  this.honeyPot--;
+}
